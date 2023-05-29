8a344098b40d7fc70a044c89050a5db2b72371ba
sofia-gilr <123768199+sofia-gilr@users.noreply.github.com>
2023-05-29 12:35:02 -0500

    avance2

--- app.js
+++ app.js
@@ -21,5 +21,20 @@ resultado = resultado +1;
 puntaje.textContent = resultado;
 posiciontopo= null;
 }
-})
-})
+});
+});
+
+function movertopo () {
+  tiempotopo = setInterval (cuadradoazar, 700);
+}
+movertopo ();
+
+function cuentaregresiva () {
+  tiempoactual--;
+  tiempofaltante.textContent = tiempoactual;
+  if (tiempoactual===0) {
+    clearInterval(idtiempo);
+    clearInterval(tiempotopo);
+    alert("se acabo el tiempo tu puntaje fue" + resultado + "topos atrapados");
+  }
+}
